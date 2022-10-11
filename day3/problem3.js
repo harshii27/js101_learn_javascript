50fce302d3de90efeb20464d6f522d971691bd8a
harshii27 <harshita00079@gmail.com>
2022-10-11 18:52:13 +0530

    addedday3

---
+++ day3/problem3.js
@@ -0,0 +1,8 @@
+//Given and character if it is a consonant print "Consonant"
+
+let char="h";
+if (char=="b" || char=="c" || char=="d" || char=="f" || char=="g" || char=="h" || char=="j" || char=="k" || char=="l" || char=="m" || char=="n"){
+  console.log("consonent");
+}else{
+  console.log("vowel");
+}
---
+++ day3/problem4.js
@@ -0,0 +1,6 @@
+//Given 3 numbers (all different values), print which is greatest
+
+let a=16;
+let b=27;
+let c=10;
+if (a>b && a>c? console.log("a is greatest"): b>c && b>a? console.log("b is greatest"): console.log("c is greatest"));
